a25c8b74cec3ca6687d1d4f5818d7c2a405f84aa
Seoyoung Kim <zinna@Zinna-Kim.local>
2026-02-13 20:37:11 -0800

    fix: fixing exports in project detail page

--- app/projectDetail/page.tsx
+++ app/projectDetail/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import NavBar from "../../public/components/NavBar.jsx";
 import Footer from "../../public/components/Footer.jsx";
