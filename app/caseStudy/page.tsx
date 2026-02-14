bbf56787e8f82b8f30f6c0313b432ca7becf1db7
Seoyoung Kim <zinna@Zinna-Kim.local>
2026-02-13 20:16:45 -0800

    fix: casestudy type error

--- app/caseStudy/page.tsx
+++ app/caseStudy/page.tsx
@@ -1,7 +1,11 @@
 export default function CaseStudy() {
-    return ( 
-        <div>
-            <h1>Case Study</h1>
-            <p>This is the case study page, under construction! Please check back later for updates.</p>
-        </div>
-    )};
+  return (
+    <div>
+      <h1>Case Study</h1>
+      <p>
+        This is the case study page, under construction! Please check back later
+        for updates.
+      </p>
+    </div>
+  );
+}
